d3eb603e2accc3174cf5e6a979209b78a5bebbde
Roger Graham <roger.graham@tuni.fi>
2022-03-17 16:09:10 +0200

    Index.js: Add e02
    2. Create new `.env` file that contains:
        DB_HOST = mydb.tamk.fi
        DB_USER =
        DB_PASSWORD =
        DB_DB =
    
    use dotenv to read the file. Do not commit the `.env` file to github. Add it to `.gitignore`.

--- homework/02/01-08/index.js
+++ homework/02/01-08/index.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql");
+require("dotenv").config();
+const dbConnection = process.env;
+// add "node":true to eslint.json if process is not defined error
+// db connection details stored in .env ..read using dotenv module
+// process.env loads the stored variables
 
 const connection = mysql.createConnection({
-  host: "mydb.tamk.fi",
-  user: "pkrogr",
-  password: "*******",
-  database: "dbpkrogr1",
+  host: dbConnection.DB_HOST,
+  user: dbConnection.DB_USER,
+  password: dbConnection.DB_PASSWORD,
+  database: dbConnection.DB_DB,
   multipleStatements: true,
 });
 
