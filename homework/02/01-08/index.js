631580d597539e1b75a77ae6af3b12b4ec65cf47
Roger Graham <roger.graham@tuni.fi>
2022-03-19 20:13:42 +0200

    Index.js: E04 amswer added
     Read about SQL Injection (wikipedia, w3schools).
     What kind of input can the user give (previous exercise) so that user may see all the content of the table? Read about escaping query values.
     Protect your app against SQL injection.

--- homework/02/01-08/index.js
+++ homework/02/01-08/index.js
@@ -1,8 +1,11 @@
-const mysql = require("mysql");
-// .env
-require("dotenv").config();
-let readlineSync = require("readline-sync");
+// SQL injection - user can add SQL commands via the user interface
+// It's a common hacking technique and can allow the user to run malicious SQL statements
+// For example input of 1; DROP table locations would delete the table locations
 
+const mysql = require('mysql');
+// .env
+require('dotenv').config();
+let readlineSync = require('readline-sync');
 const dbConnection = process.env;
 // add "node":true to eslint.json if process is not defined error
 // db connection details stored in .env ..read using dotenv module
@@ -13,18 +16,29 @@ const connection = mysql.createConnection({
   user: dbConnection.DB_USER,
   password: dbConnection.DB_PASSWORD,
   database: dbConnection.DB_DB,
-  multipleStatements: true,
+  multipleStatements: false,
+  // multiple statements: false stops the user adding additional SQL statements in their input
+  // for example ; DROP table locations won't be accepted as it's an additional statement
 });
 
 function searchId() {
-  let locationId = readlineSync.question("May I have an ID ? ");
-  let sql = `SELECT * FROM locations WHERE id = ${locationId}`;
+  let id = readlineSync.question('May I have an ID ? ');
+  // let sql = `select * from locations where id = ${connection.escape(id)} `;
+  // use connection.escape to sanitise or alternatively us a placholder char - ?
+  let sql = `select * from locations where id = ? `;
+
   connection.connect();
-  connection.query(sql, (err, location) => {
+  //connection.escape
+  // connection.query(sql, (err, locations) => {
+
+  // ? value passed to an array, can hold the values of multiple placeholder chars [Placeholder1, Placeholder2...]
+  connection.query(sql, [id], (err, locations) => {
     if (err) {
       throw err;
     }
-
+    //initialise location as first item in an array locations
+    // otherwise escape fails
+    let location = locations[0];
     console.log(
       `ID: ${location.id} Latitude: ${location.latitude} Longtitude: ${location.longtitude}`
     );
