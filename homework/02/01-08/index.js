c233caab7a3dc4f41b50d9eb0e694c5160af5c72
Roger Graham <roger.graham@tuni.fi>
2022-03-22 18:29:49 +0200

    e06: Change answer
    Index.js: Change connection.close()
    
    function now called using await

--- homework/02/01-08/index.js
+++ homework/02/01-08/index.js
@@ -7,8 +7,7 @@ const main = async () => {
   let findId = await connection.findById(42, () => findId);
   let addLocation = await connection.save([20, 50], () => addLocation);
   let deleteLocation = await connection.deleteById(90, () => deleteLocation);
-
-  connection.close();
+  await connection.close();
 };
 
 main();
