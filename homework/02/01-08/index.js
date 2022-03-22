6af83afe5dece79b6629a4e6e2ae6a93d08d8bff
Roger Graham <roger.graham@tuni.fi>
2022-03-22 21:15:09 +0200

    e08: Add answer
    Create proper error handling for all methods. Call resolve and proper error messages.
    
    File: Index.js change

--- homework/02/01-08/index.js
+++ homework/02/01-08/index.js
@@ -1,13 +1,30 @@
 const connection = require('./database/crudrepository.js');
 
 const main = async () => {
-  await connection.connect();
-  // using returns avoids esLint warning about unused variables
-  let results = await connection.findAll(() => results);
-  let findId = await connection.findById('s', () => findId);
-  let addLocation = await connection.save([-201, 200], () => addLocation);
-  let deleteLocation = await connection.deleteById(108, () => deleteLocation);
-  await connection.close();
+  try {
+    let results = await connection.findAll(() => results);
+    // let findId = await connection.findById("a", () => findId);
+    let findId = await connection.findById(15, () => findId);
+    let addLocation = await connection.save([-201, 200], () => addLocation);
+    let deleteLocation = await connection.deleteById(108, () => deleteLocation);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    try {
+      console.log(await connection.close());
+    } catch (err) {
+      console.log(err);
+    }
+  }
 };
 
+// const main = async () => {
+//   await connection.connect();
+//   // using returns avoids esLint warning about unused variables
+//   let results = await connection.findAll(() => results);
+//   let findId = await connection.findById('s', () => findId);
+//   let addLocation = await connection.save([-201, 200], () => addLocation);
+//   let deleteLocation = await connection.deleteById(108, () => deleteLocation);
+//   await connection.close();
+// };
 main();
