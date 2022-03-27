bb17f9b556f5c0cc9df12cb4ec2078f723affb0e
Roger Graham <roger.graham@tuni.fi>
2022-03-27 16:02:08 +0300

    e04: implemented
    locations: change
    Add the possibility to use HTTP GET, POST and DELETE. These curl commands should work and should response some dummy data (“just text”)
    
    app.js change: port = 8080

--- homework/e03/routes/locations.js
+++ homework/e03/routes/locations.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const locations = express.Router();
+// const location1 = { id: 1, latitude: 60, longitude: 60 };
+// const location2 = { id: 2, latitude: 40, longitude: 80 };
 
-const location = { id: 1, latitude: 60, longitude: 60 };
-locations.get("/1", (req, res) => {
-  res.send(location);
+locations.get("/", (req, res) => {
+  res.send("Getting All locations");
 });
+
+locations.get("/1", (req, res) => {
+  res.send("Getting location with id 1");
+});
+
+locations.get("/2", (req, res) => {
+  res.send("Getting location with id 2");
+});
+
+locations.post("/", (req, res) => {
+  res.send("Adding new location");
+});
+
+locations.delete("/1", (req, res) => {
+  res.send("Delete location with id 1");
+});
+
 locations.use("/location", locations);
 
 module.exports = locations;
